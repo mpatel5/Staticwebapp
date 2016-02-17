54b5c0756dd14238c3e6fbe66282d15806567b42
Mitesh Patel <mpatel5@icloud.comgit config --global user.email mpatel5@icloud.comgit config --global user.email mpatel5@icloud.comgit config --global user.name Mitesh>
2016-02-17 22:06:37 +0000

    Initial commit for heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Static::Application.config.secret_key_base = 'd0300d8857bc5bfe63e47972eed377ea2734f39a6d35354ec027d5e0466dd21f46fa72b0c289561c73a148b1b6578ad00f7a085fc0bbc909f9c5533551ffec13'
